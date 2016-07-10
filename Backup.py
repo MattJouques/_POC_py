e6db81f3fff9caf1ac3ac531cda97d56eeaf3403
MattJouques <Matt@omega.home>
2016-07-10 21:49:29 +0100

    Framework of file created

---
+++ Backup.py
@@ -0,0 +1,32 @@
+#!/usr/local/bin/python2.7
+# encoding: utf-8
+'''
+Backup is a python script used to backup local files to a remote drive
+
+@author:     Matt Jouques
+
+@copyright:  2016 Matt Jouques. All rights reserved.
+
+@license:    Apache License 2.0
+
+@contact:    matt@jouques.net
+@deffield    updated: 10 July 2016
+'''
+
+# Configuration
+    # Define the files to backup
+    # Define the destination for the backup
+        # Local
+        # Remote   
+    # Set the backup Variables
+        # Local Machine / Instance
+        # Date & Timestamp for the backup
+        # Archiving
+    # Logging
+    # Notifications
+
+# Backup
+    # Files
+    # Database
+    
+# Error Handling    
