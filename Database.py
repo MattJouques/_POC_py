0a2289a5a6719847d9fed3d5cd406c022fada09f
MattJouques <Matt@omega.home>
2016-07-10 08:10:47 +0100

    Initial Commit

---
+++ Database.py
@@ -0,0 +1,32 @@
+#
+#    POC - Database Connectivity
+#
+#    @author: Matt
+#
+
+# Configuration
+dbConfig = {
+  'user': 'development',
+  'password': 'd3v3l0pm3nt',
+  'host': 'alpha.local',
+  'database': 'development',
+  'raise_on_warnings': True,
+}
+
+# Dependencies
+import mysql.connector
+
+# Main
+try:
+    db = mysql.connector.connect(**dbConfig)
+    # prepare a cursor object using cursor() method
+    cursor = db.cursor()
+    # execute SQL query using execute() method.
+    cursor.execute("SELECT VERSION()")
+    # Fetch a single row using fetchone() method.
+    data = cursor.fetchone()
+    print "Database version : %s " % data
+except mysql.connector.Error as err:
+    print(err)
+else:
+    db.close()
---
+++ Filesystem.py
@@ -0,0 +1,5 @@
+'''
+Created on 10 Jul 2016
+
+@author: Matt
+'''
